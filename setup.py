b87bc33fa069b2643e8ba4d8081381fde93590ac
eerorika <eerorika@gmail.com>
2020-01-13 04:47:47 +0200

    added dependencies

--- setup.py
+++ setup.py
@@ -3,13 +3,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='pullrequest',
-    version='0.1.0',
+    version='0.1.1',
     author='Eero Rikalainen',
     author_email='eerorika@gmail.com',
     url='https://github.com/eerorika/pullrequest',
     description='Create pullrequest to git service',
     license='MIT',
     packages=find_packages(),
+    install_requires=[
+        'PyGithub',
+        'python-gitlab',
+        'requests_oauthlib',
+    ],
     entry_points={
         'console_scripts': [
             'pullrequest = pullrequest.cli:main'
