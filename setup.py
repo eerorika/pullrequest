4eabc7b61f590c295271fbd380fd24ad66d783c3
eerorika <eerorika@gmail.com>
2020-01-31 02:37:55 +0200

    0.1.4 fix commit argument

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pullrequest',
-    version='0.1.3',
+    version='0.1.4',
     author='Eero Rikalainen',
     author_email='eerorika@gmail.com',
     url='https://github.com/eerorika/pullrequest',
